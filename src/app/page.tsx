e01005868b721e5f997c3856d4ed9266a043cf24
mw665072-lab <mw665072@gmail.com>
2025-10-28 12:06:53 +0500

    remove scroll indicator and bg in hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,22 +23,26 @@ export default function Home() {
       <div className="py-6 px-10">
         <HeaderSection />
       </div>
-      <div className="py-6 px-10 bg-gray-800 ">
+      <div className="py-6 px-10 bg-gray-800 flex flex-col gap-16">
         {HeroSection_ue.map((section, index) => (
           <HeroSection key={index} title={section.title} description={section.description} variant={section.variant} />
         )
         )}
-      </div>
-      <div className="py-6 px-10">
-        <div className="grid gap-4 md:gap-6 grid-cols-[repeat(auto-fit,minmax(350px,1fr))]">
-          {FEATURE_CARDS.map((card, index) => (
-            <FeatureCard key={index} title={card.title} description={card.description} variant={card.variant} />
-          ))}
+        <div className="px-10">
+          <div className="grid gap-4 md:gap-6 grid-cols-[repeat(auto-fit,minmax(350px,1fr))]">
+            {FEATURE_CARDS.map((card, index) => (
+              <FeatureCard key={index} title={card.title} description={card.description} variant={card.variant} />
+            ))}
+          </div>
+        </div>
+        <div className="px-10">
+          <LogoCarousel logos={PARTNER_LOGOS} />
         </div>
       </div>
-      <div className="py-6 px-10">
-        <LogoCarousel logos={PARTNER_LOGOS} />
+      <div>
+
       </div>
+
     </div>
   );
 }
