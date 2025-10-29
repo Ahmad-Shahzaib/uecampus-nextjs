4fc713cf5e4ee962e1876051378e3f6bb0fe0972
mw665072-lab <mw665072@gmail.com>
2025-10-29 14:35:24 +0500

    fix and responsive header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,13 +27,13 @@ export default function Home() {
 
   return (
     <div>
-      <div className="py-6 px-10 bg-gray-800 flex flex-col space-y-8">
+      <div className="py-6  bg-gray-800 flex flex-col space-y-8">
         {HeroSection_ue.map((section, index) => (
           <HeroSection key={index} title={section.title} description={section.description} variant={section.variant} />
         )
         )}
-        <div className="flex h-auto justify-center">
-          <div className="grid gap-4 md:gap-6 grid-cols-[repeat(auto-fit,minmax(280px,1fr))] ">
+        <div className="flex h-auto justify-center px-10">
+          <div className="grid gap-4 md:gap-6 grid-cols-[repeat(auto-fit,minmax(280px,1fr))] w-full">
             {FEATURE_CARDS.map((card, index) => (
               <FeatureCard
                 key={index}
@@ -49,8 +49,7 @@ export default function Home() {
           <LogoCarousel logos={PARTNER_LOGOS} />
         </div>
       </div>
-      <div>
-      </div>
+     
       <div>
         <CouresSection />
       </div>
