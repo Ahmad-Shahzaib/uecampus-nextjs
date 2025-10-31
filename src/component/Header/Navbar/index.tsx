5b3626826886e10f302284e558099ffa141cab4c
mw665072-lab <mw665072@gmail.com>
2025-10-31 18:05:30 +0500

    fix reload issues

--- src/component/Header/Navbar/index.tsx
+++ src/component/Header/Navbar/index.tsx
@@ -17,72 +17,69 @@ type Props = {
 const NavigationSection: React.FC<Props> = ({ mobile = false }) => {
   const [aboutOpen, setAboutOpen] = useState(false);
 
-if (mobile) {
-  return (
-    <ul className="flex flex-col gap-2 w-full text-gray-800 font-medium relative">
-      <li className="relative">
-        <button
-          aria-expanded={aboutOpen}
-          onClick={() => setAboutOpen((s) => !s)}
-          className="w-full flex items-center  text-left py-2 px-1"
-        >
-          <span>About Us</span>
-          <span className="ml-2">
-            {aboutOpen ? <ChevronUp size={16} /> : <ChevronDown size={16} />}
-          </span>
-        </button>
-    
-        {aboutOpen && (
-          <ul
-            className="absolute left-0 top-full mt-1 w-[260px] bg-white shadow-lg rounded-lg border border-gray-100 p-4 z-50"
+  if (mobile) {
+    return (
+      <ul className="flex flex-col gap-2 w-full text-gray-800 font-medium relative">
+        <li className="relative">
+          <button
+            aria-expanded={aboutOpen}
+            onClick={() => setAboutOpen((s) => !s)}
+            className="w-full flex items-center text-left py-2 px-1"
           >
-            <li>
-              <Link
-                href="/about-us"
-                className="block py-1 hover:text-purple-600 transition-colors"
-              >
-                About UeCampus
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/accreditation-partners"
-                className="block py-1 hover:text-purple-600 transition-colors"
-              >
-                Accreditation &amp; Partners
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/faqs"
-                className="block py-1 hover:text-purple-600 transition-colors"
-              >
-                Frequently Asked Questions
-              </Link>
-            </li>
-          </ul>
-        )}
-      </li>
+            <span>About Us</span>
+            <span className="ml-2">
+              {aboutOpen ? <ChevronUp size={16} /> : <ChevronDown size={16} />}
+            </span>
+          </button>
 
-      <li>
-        <Link href="/programmes" className="block py-2">
-          Programmes & Diploma
-        </Link>
-      </li>
-      <li>
-        <Link href="/scholarship" className="block py-2">
-          Scholarship
-        </Link>
-      </li>
-      <li>
-        <Link href="/contact-us" className="block py-2">
-          Contact Us
-        </Link>
-      </li>
-    </ul>
-  );
-}
+          {aboutOpen && (
+            <ul className="absolute left-0 top-full mt-1 w-[260px] bg-white shadow-lg rounded-lg border border-gray-100 p-4 z-50">
+              <li>
+                <Link
+                  href="/about-us"
+                  className="block py-1 hover:text-purple-600 transition-colors"
+                >
+                  About UeCampus
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/accreditation-partners"
+                  className="block py-1 hover:text-purple-600 transition-colors"
+                >
+                  Accreditation &amp; Partners
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/faqs"
+                  className="block py-1 hover:text-purple-600 transition-colors"
+                >
+                  Frequently Asked Questions
+                </Link>
+              </li>
+            </ul>
+          )}
+        </li>
 
+        <li>
+          <Link href="/programmes" className="block py-2">
+            Programmes & Diploma
+          </Link>
+        </li>
+        <li>
+          <Link href="/scholarship" className="block py-2">
+            Scholarship
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact-us" className="block py-2">
+            Contact Us
+          </Link>
+        </li>
+      </ul>
+    );
+  }
 
   return (
     <NavigationMenu>
@@ -91,23 +88,37 @@ if (mobile) {
           <NavigationMenuTrigger>About Us</NavigationMenuTrigger>
           <NavigationMenuContent>
             <div className="grid gap-3 p-4 w-[300px]">
-              <NavigationMenuLink href="/about-us">About UeCampus</NavigationMenuLink>
-              <NavigationMenuLink href="/accreditation-partners">Accreditation &amp; Partners </NavigationMenuLink>
-              <NavigationMenuLink href="/faqs">Frequently Asked Questions</NavigationMenuLink>
+              <NavigationMenuLink asChild>
+                <Link href="/about-us">About UeCampus</Link>
+              </NavigationMenuLink>
+              <NavigationMenuLink asChild>
+                <Link href="/accreditation-partners">
+                  Accreditation &amp; Partners
+                </Link>
+              </NavigationMenuLink>
+              <NavigationMenuLink asChild>
+                <Link href="/faqs">Frequently Asked Questions</Link>
+              </NavigationMenuLink>
             </div>
           </NavigationMenuContent>
         </NavigationMenuItem>
 
         <NavigationMenuItem>
-          <NavigationMenuLink href="/courses">Programmes & Diploma</NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link href="/courses">Programmes & Diploma</Link>
+          </NavigationMenuLink>
         </NavigationMenuItem>
 
         <NavigationMenuItem>
-          <NavigationMenuLink href="/scholarship">Scholarship</NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link href="/scholarship">Scholarship</Link>
+          </NavigationMenuLink>
         </NavigationMenuItem>
 
         <NavigationMenuItem>
-          <NavigationMenuLink href="/contact-us">Contact Us</NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link href="/contact-us">Contact Us</Link>
+          </NavigationMenuLink>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
