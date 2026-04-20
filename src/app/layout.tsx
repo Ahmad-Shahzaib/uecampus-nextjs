4b8a90d980f05a04f07a74d3fed5e797f46fc021
hashir-lang <hashir@uecampus.com>
2026-04-20 14:12:24 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,17 @@ export default function RootLayout({
                   `
                 }} />
                 {/* End Google Tag Manager */}
+                {/* Google Tag Manager */}
+                <script dangerouslySetInnerHTML={{
+                  __html: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-MSV324H2');
+                  `
+                }} />
+                {/* End Google Tag Manager */}
                 {/* Meta Pixel Code */}
                 <script dangerouslySetInnerHTML={{
                   __html: `
@@ -140,6 +151,12 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NTNRZXX"
+                  height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MSV324H2"
                   height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
